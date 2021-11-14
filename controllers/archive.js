7b2e9d089ba6e323ba7873d4fcb976ab98fae439
TeflonTrout <jtkazo@gmail.com>
2021-11-14 11:52:03 -0500

    testing new route

--- controllers/archive.js
+++ controllers/archive.js
@@ -6,14 +6,14 @@ const router = express.Router();
 
 export const archivePost = async (req, res) => {
     console.log(req.params.id)
-    const { movieTitle, creator, releaseDate, poster, priority, submittedOn, value } = req.body;
-    const newArchivePost = new ArchivePost({ movieTitle, creator, releaseDate, poster, priority, submittedOn, value })
-    try {
-        await newArchivePost.save();
-        res.status(201).json(newArchivePost)
-    } catch (error) {
-        res.status(409).json({message: error.message})
-    }
+    // const { movieTitle, creator, releaseDate, poster, priority, submittedOn, value } = req.body;
+    // const newArchivePost = new ArchivePost({ movieTitle, creator, releaseDate, poster, priority, submittedOn, value })
+    // try {
+    //     await newArchivePost.save();
+    //     res.status(201).json(newArchivePost)
+    // } catch (error) {
+    //     res.status(409).json({message: error.message})
+    // }
 }
 
 export const getArchive = async (req, res) => {
