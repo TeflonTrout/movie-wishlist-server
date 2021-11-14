f4578a6813fef59dc5c4c43f79a4f21647ae95bb
TeflonTrout <jtkazo@gmail.com>
2021-11-14 13:50:37 -0500

    simplify schema

--- models/archivePost.js
+++ models/archivePost.js
@@ -2,19 +2,18 @@ import mongoose from 'mongoose';
 import uniqueValidator from 'mongoose-unique-validator';
 
 const archiveShema = mongoose.Schema({
-    id: String,
+    // id: String,
     movieTitle: {type: String, required: true, unique: true},
-    creator: String,
-    priority: String,
-    releaseDate: String,
-    poster: String,
-    value: Number,
-    submittedOn: String,
-    __v: Number,
-    _id: String
+    // creator: String,
+    // priority: String,
+    // releaseDate: String,
+    // poster: String,
+    // value: Number,
+    // submittedOn: String,
+    // _id: String
 })
 
-// archiveShema.plugin(uniqueValidator);
+archiveShema.plugin(uniqueValidator);
 const ArchivePost = mongoose.model('ArchivePost', archiveShema)
 
 export default ArchivePost
