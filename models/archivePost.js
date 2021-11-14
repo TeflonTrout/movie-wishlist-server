92c8e33845f34c2c04cc86b61e278d427cce349e
TeflonTrout <jtkazo@gmail.com>
2021-11-14 14:03:18 -0500

    updated schema

--- models/archivePost.js
+++ models/archivePost.js
@@ -5,12 +5,9 @@ const archiveShema = mongoose.Schema({
     id: String,
     movieTitle: {type: String, required: true, unique: true},
     creator: String,
-    priority: String,
     releaseDate: String,
     poster: String,
-    value: Number,
     submittedOn: String,
-    // _id: String
 })
 
 archiveShema.plugin(uniqueValidator);
